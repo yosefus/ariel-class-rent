5f55e740d835851d01219592911ddec96327872d
yosefus <yosalsoni@gmail.com>
2024-09-09 19:09:48 +0300

    metadata img

--- app/houses/[houseName]/page.jsx
+++ app/houses/[houseName]/page.jsx
@@ -3,6 +3,7 @@ import data from '@/data/data.json';
 import { wait } from '@/helpers/wait';
 import { readHouseService, readHousesService } from '@/server/BL/service/house.service';
 import { connectToMongo } from '@/server/connect';
+import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
 
@@ -12,20 +13,31 @@ import { notFound } from 'next/navigation';
 //    return allHouses.map(house => ({ houseName: house.name }))
 // }
 
+export const generateMetadata = async ({ params: { houseName } }) => {
+   await connectToMongo()
+   const currentHouse = await readHouseService({ name: decodeURI(houseName) })
+   return ({
+      title: currentHouse.name, 
+      description: currentHouse.description
+})
+}
+
 export default async function House({ params: { houseName } }) {
    await connectToMongo()
    // await wait()
-   
-   const currentHouse = await readHouseService({name: decodeURI(houseName)})
+
+   const currentHouse = await readHouseService({ name: decodeURI(houseName) })
 
    if (!currentHouse) {
       notFound()
    }
 
    return (
-      <div>
+      <div style={{position: 'relative', color: '#fff', height: '90vh'}}>
+         <Image src={currentHouse.img} style={{objectFit: 'cover', zIndex: -1}}  alt={currentHouse.name} fill />
          <h1>{currentHouse.name}</h1>
          <h2>${currentHouse.price}</h2>
+         <p>{currentHouse.description}</p>
       </div>
    )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   images: {
+      remotePatterns: [
+        {
+          hostname: 'ca.slack-edge.com',
+        },
+      ],
+    },
+};
 
 export default nextConfig;
